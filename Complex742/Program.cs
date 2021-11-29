68fc713e063a8246dd940358a6b5f74465018451
Anton Zhavoronkov <zhavoronkov0@mail.ru>
2021-11-29 21:46:32 +0500

    Убрано все, что создается шаблоном. В том числе логгирование и css

--- Complex742/Program.cs
+++ Complex742/Program.cs
@@ -1,33 +1,22 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Complex742.Data;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<Complex742Context>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("Complex742Context")));
+builder.Services.AddTransient<IEquipmentRepository, DbEquipmentRepository>();
 
-    builder.Services.AddTransient<IEquipmentRepository, DbEquipmentRepository>();
-
-
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
-app.UseAuthorization();
 
 app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
 
